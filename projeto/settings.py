377786999aafb87beb679c63ca95bb9f461c7255
Andre G. Oliveira <rolandolero429@gmail.com>
2023-06-26 17:22:07 -0300

    ver 0.1

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2dk$5b+a*ab5(rxg2png8s+6g5wev5^di@m&9du!ew(=d)vgnn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
--- recipes/views.py
+++ recipes/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
+def home(request):
+    return render(request,'home.html')
+
 def sobre(request):
     return HttpResponse('sobre')
-
-def home(request):
-    return HttpResponse('home')
